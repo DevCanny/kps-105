1fb4aa6daf1cbab23d61a3abfed9ea6f7b4edabb
DevCanny <79893805+DevCanny@users.noreply.github.com>
2021-08-23 18:08:49 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,4 @@
+//db.Players.find({"id.name": ""}).limit(5).pretty()
+//db.Players.find({"dob.age": {$gt: 49}}).limit(150).pretty()
+//db.Players.find({"name.first": {$regex: /^meh/ig}}).pretty()
+//
